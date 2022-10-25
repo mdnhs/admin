d5c6433bda0777de42e11dd9af0a897d1a6da470
mdnhs <mdnhs.cse@gmail.comm>
2022-10-25 17:43:09 +0600

    4th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FiShoppingCart } from "react-icons/fi";
 import { BsChatLeft } from "react-icons/bs";
@@ -20,15 +20,40 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
+      />
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 );
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
@@ -68,11 +93,17 @@ const Navbar = () => {
             <p>
               <span className="text-gray-400 text-14">Hi,</span>
               {""}
-              <span className="text-gray-400 font-medium ml-1 text-14">Sourab</span>
+              <span className="text-gray-400 font-medium ml-1 text-14">
+                Sourab
+              </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,12 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSidebar = () => { 
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
   const activeLink =
     "flex items-center gap-5 text-white pl-4 pt-3 py-2.5 rounded-lg text-md m-2";
   const normalLink =
@@ -20,7 +25,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware className="text-2xl" /> <span>DigiGate360</span>
@@ -45,7 +50,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSidebar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
